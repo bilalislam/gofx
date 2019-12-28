dc935b499cc4575eca7edc90c6b09ddfeda51862
bilalislam <bilal@tragate.com>
2019-12-29 00:48:59 +0300

    consumer has been refactored

--- examples/main.go
+++ examples/main.go
@@ -1,63 +1,79 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"github.com/emretiryaki/rabbitmq"
 	"github.com/go-redis/redis"
 	"gofx/consumer"
 	"gofx/repository"
 	"gofx/repository/clients"
-	"log"
+	"time"
 )
 
 var (
-	uri          = flag.String("uri", "amqp://guest:guest@localhost:5672/", "AMQP URI")
-	exchange     = flag.String("exchange", "test-exchange", "Durable, non-auto-deleted AMQP exchange name")
-	exchangeType = flag.String("exchange-type", "direct", "Exchange type - direct|fanout|topic|x-custom")
-	queue        = flag.String("queue", "test-queue", "Ephemeral AMQP queue name")
-	bindingKey   = flag.String("key", "test-key", "AMQP binding key")
-	consumerTag  = flag.String("consumer-tag", "simple-consumer", "AMQP consumer tag (should not be blank)")
-	environment  = flag.String("environment", "dev", "os environment")
+	uri           = flag.Args()
+	username      = flag.String("username", "guest", "username")
+	password      = flag.String("password", "guest", "password")
+	retryCount    = flag.Int("retry-count", 1, "retry count")
+	prefetchCount = flag.Int("prefetch-count", 10, "prefetch count")
+	exchange      = flag.String("exchange", "test-exchange", "Durable, non-auto-deleted AMQP exchange name")
+	exchangeType  = flag.Int("exchange-type", 3, "Exchange type -> 1-direct,2-fanout,3-topic,4-ConsistentHashing,5-XDelayedMessage")
+	queue         = flag.String("queue", "test-queue", "Ephemeral AMQP queue name")
+	bindingKey    = flag.String("key", "test-key", "AMQP binding key")
 )
 
 func init() {
 	flag.Parse()
 }
 
+type (
+	PersonV1 struct {
+		Name    string
+		Surname string
+		City    City
+		Count   int
+	}
+
+	City struct {
+		Name string
+	}
+)
+
 func main() {
 
-	c, err := consumer.NewConsumer(consumer.Request{
-		Uri:          *uri,
-		Exchange:     *exchange,
-		ExchangeType: *exchangeType,
-		Queue:        *queue,
-		RoutingKey:   *bindingKey,
-		ConsumerTag:  *consumerTag,
+	client := &repository.Client{
+		Context: &clients.RedisClient{
+			Conn: redis.NewClient(&redis.Options{}),
+		},
+	}
+
+	onConsumed := func(message rabbitmq.Message) error {
+		err := client.Add(message, 0)
+		var consumeMessage PersonV1
+		err = json.Unmarshal(message.Payload, &consumeMessage)
+		client.Add(consumeMessage, time.Duration(0))
+		if err != nil {
+			return err
+		}
+		fmt.Println(time.Now().Format("Mon, 02 Jan 2006 15:04:05 "), " Message:", consumeMessage)
+
+		return nil
+	}
+
+	r, c := consumer.AddConsumer(consumer.Request{
+		Uri:           []string{"127.0.0.1:5672", "127.0.0.1:5673", "127.0.0.1:5672"},
+		UserName:      *username,
+		Password:      *password,
+		RetryCount:    *retryCount,
+		PrefetchCount: *prefetchCount,
+		Exchange:      *exchange,
+		ExchangeType:  *exchangeType,
+		Queue:         *queue,
+		RoutingKey:    *bindingKey,
 	})
 
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-
-	messages, err := c.Consume(*queue)
-	for d := range messages {
-		log.Printf(
-			"got %dB delivery: [%v] %s",
-			len(d.Body),
-			d.DeliveryTag,
-			d.Body,
-		)
-
-		client := &repository.Client{
-			Context: &clients.RedisClient{
-				Conn: redis.NewClient(&redis.Options{}),
-			},
-		}
-
-		err := client.Add(d.Body, 0)
-		if err == nil {
-			_ = d.Ack(true)
-		} else {
-			panic(err)
-		}
-	}
+	c.HandleConsumer(onConsumed)
+	r.RunConsumers()
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/emretiryaki/rabbitmq v0.0.0-20190810052556-943a6293cf45
 	github.com/go-redis/redis v6.15.6+incompatible
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/labstack/echo/v4 v4.0.0
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/onsi/ginkgo v1.11.0 // indirect
